22dee2bc01866f0104f16fba411f0d7dc55c8bcb
root <root@smoelenboek-drupal-prd-001.naturalis.nl>
2018-11-01 07:59:38 +0000

    Improved import

--- smoelimport.sh
+++ smoelimport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 drush vset feeds_process_limit 2000
-drush delete-all smoel
+drush delete-all all --reset
 drush feeds-im smoel_xml -y
-yes | sudo drush search-reindex --immediate
+yes | drush search-reindex --immediate
 drush cc all
