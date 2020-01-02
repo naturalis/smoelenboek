7e7b1bf7d6f95fcfcb7ebe8a7e61fa64cceb8626
Joep Vermaat <joep.vermaat@naturalis.nl>
2020-01-02 15:31:10 +0100

    Copy images van de X naar de D schijf

--- windows/scripts/sync_smoelenboek.bat
+++ windows/scripts/sync_smoelenboek.bat
@@ -1,6 +1,7 @@
 REM Hier wordt gebruik gemaakt van rsync om diverse bestanden vanaf windows omgevingen naar Unix/linux te kopieren
 REM
 REM kopieer de bestanden vanaf de fs-smb-002.ad.naturalis.nl
+COPY X:\_org\* D:\Data\Smoelenboek\
 C:\cygwin64\bin\rsync.exe -av --chmod=744 -e  "C:\cygwin64\bin\ssh.exe -E C:\Beheer\rsync\Log\ssh-images.log -i C:\beheer\rsync\id_rsa -o StrictHostKeyChecking=no " /cygdrive/d/Data/Smoelenboek/ smoelenboek.sync@145.136.242.214:/data/images
 
 REM kopieer C:\Smoelenboek\smoels.xml vanaf hier ( MWB-AS-002.ad.naturalis.nl)
